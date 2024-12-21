1448e6d60dc314e14e7c00e5f099119a5e10e519
Magno Jovino <magno.jovino@hotmail.com>
2024-12-21 01:51:36 -0300

    feat: add word-lengths-horizontal-histogram

---
+++ chapter-one/word-lengths-horizontal-histogram/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main() {
+  int word_length = 0;
+  int words_was_sent = 0;
+  int header_was_printed = 0;
+  int c;
+
+  while ((c = getchar()) != EOF) {
+    if (c == '\n' || c == '\t' || c == ' ') {
+      if (!words_was_sent) {
+        printf("###############################\n");
+        printf("# WORLDS HORIZONTAL HISTOGRAM #\n");
+        printf("###############################\n");
+
+        words_was_sent = 1;
+      } else {
+        printf(": ");     
+      }
+
+      while (word_length > 0) {
+        printf("*");
+        --word_length;
+      }
+
+      printf("\n");
+    } else {
+      if (!words_was_sent) {
+        printf("###############################\n");
+        printf("# WORLDS HORIZONTAL HISTOGRAM #\n");
+        printf("###############################\n");
+
+        words_was_sent = 1;
+      }
+      
+      putchar(c);
+
+      ++word_length;
+    }
+  }
+}
